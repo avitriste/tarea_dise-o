44d195cdeb35dc2ebe645d992413e7f66bfd108a
Avriluchis <avril.monterrubio@anima.edu.uy>
2024-09-10 09:32:12 -0300

    subiendo las cositas

---
+++ react-visualizacion-usuarios/src/Home.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Página de Inicio</h1>
+      <p>Utiliza los enlaces</p>
+    </div>
+  );
+}
+
+export default Home;
